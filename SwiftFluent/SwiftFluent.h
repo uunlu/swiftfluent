9e4c5c22d36942cf15e1c3fad8bf395ea04c82e1
Ugur Unlu <uguru@backbase.com>
2023-07-31 13:13:21 +0200

    Initial Commit

---
+++ SwiftFluent/SwiftFluent.h
@@ -0,0 +1,18 @@
+//
+//  SwiftFluent.h
+//  SwiftFluent
+//
+//  Created by Ugur Unlu on 31/07/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftFluent.
+FOUNDATION_EXPORT double SwiftFluentVersionNumber;
+
+//! Project version string for SwiftFluent.
+FOUNDATION_EXPORT const unsigned char SwiftFluentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFluent/PublicHeader.h>
+
+
