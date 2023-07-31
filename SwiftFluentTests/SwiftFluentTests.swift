0f29b3ecb31e3b022e1964b685075d5233a1ae5c
Ugur Unlu <uguru@backbase.com>
2023-07-31 13:59:35 +0200

    test password is empty and length

--- SwiftFluentTests/SwiftFluentTests.swift
+++ SwiftFluentTests/SwiftFluentTests.swift
@@ -23,4 +23,16 @@ final class SwiftFluentTests: XCTestCase {
         let result = sut.validate("")
         XCTAssertFalse(result.isValid)
     }
+
+    func testOnStringNotEmptyAndLongerThanRequiredCharacter_IsValid() throws {
+        let password = "myValidPassword"
+        let sut = Validator<String>()
+
+        sut
+            .validate({ !$0.isEmpty}, errorMessage: "Should not be empty")
+            .validate({ $0.count > 5 }, errorMessage: "Should be at least 6 characters")
+        
+        let result = sut.validate(password)
+        XCTAssertEqual(result.isValid, true, "Expected true but invalid password")
+    }
 }
