fe7e0531953a05a0286d0ccd3e6fb12393d5bf77
Ugur Unlu <uguru@backbase.com>
2023-08-02 12:00:14 +0200

    Add comments for greaterThan

--- SwiftFluent/Validator/RuleForBuilder/RuleForBuilder+Comparable.swift
+++ SwiftFluent/Validator/RuleForBuilder/RuleForBuilder+Comparable.swift
@@ -69,21 +69,55 @@ public extension RuleForBuilder where Value: Comparable {
 // MARK: - greaterThan
 
 public extension RuleForBuilder where Value: Comparable {
-    func greaterThan(_ maxValue: Value, errorMessage: String? = nil) -> RuleForBuilder<Model, Value> {
-        buildGreaterthan(errorMessage, maxValue)
+    /**
+     Adds a validation rule to check if the value of the property is greater than the specified `maxValue`.
+
+     - Parameter minValue: The minimum value allowed for the property.
+     - Parameter errorMessage: The error message to display if the validation fails. If not provided, a default error message will be used.
+     - Returns: The `RuleForBuilder` instance to allow method chaining for further rule definitions.
+
+     Example usage:
+     ```
+     let validator = Validator<User>()
+     .ruleFor(.age)
+     .greaterThan(18, errorMessage: "Age must be greater than 18.")
+     .ruleFor(.score)
+     .greaterThan(0) // Uses the default error message.
+     ```
+     - Note: If the `errorMessage` is not provided, a default error message will be used in the format: "The value of 'KeyPath' must be greater than 'minValue'.". The actual property name and `minValue` will be dynamically inserted into the error message.
+     */
+    func greaterThan(_ minValue: Value, errorMessage: String? = nil) -> RuleForBuilder<Model, Value> {
+        buildGreaterthan(errorMessage, minValue)
         return self
     }
 
-    func greaterThan(_ maxValue: Value, errorMessage: String? = nil) -> Validator<Model> {
-        buildGreaterthan(errorMessage, maxValue)
+    /**
+     Adds a validation rule to check if the value of the property is greater than the specified `minValue`.
+
+     - Parameter minValue: The minimum value allowed for the property.
+     - Parameter errorMessage: The error message to display if the validation fails. If not provided, a default error message will be used.
+     - Returns: The `Validator` instance to allow further rule definitions for different properties.
+
+     Example usage:
+     ```
+     let validator = Validator<User>()
+     .ruleFor(.age)
+     .greaterThan(18, errorMessage: "Age must be greater than 18.")
+     .ruleFor(.score)
+     .greaterThan(0) // Uses the default error message.
+     ```
+     - Note: If the `errorMessage` is not provided, a default error message will be used in the format: "The value of 'KeyPath' must be greater than 'minValue'.". The actual property name and `minValue` will be dynamically inserted into the error message.
+     */
+    func greaterThan(_ minValue: Value, errorMessage: String? = nil) -> Validator<Model> {
+        buildGreaterthan(errorMessage, minValue)
         return validator
     }
 
-    fileprivate func buildGreaterthan(_ errorMessage: String?, _ maxValue: Value) {
-        let error = errorMessage ?? "‘\(keyPath.propertyName)’ must be greater than \(maxValue)."
+    fileprivate func buildGreaterthan(_ errorMessage: String?, _ minValue: Value) {
+        let error = errorMessage ?? "‘\(keyPath.propertyName)’ must be greater than \(minValue)."
         let rule = ValidationRule<Model>(errorMessage: {error}) { model in
             let propertyValue = model[keyPath: keyPath]
-            return propertyValue > maxValue
+            return propertyValue > minValue
         }
         validator.addRule(rule)
     }
